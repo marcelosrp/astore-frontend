9b272988cb4a8eb559d02498d348731dca7941b5
Marcelo Soares <soares.srp@gmail.com>
2023-08-26 14:47:17 -0300

    feat: script to open cases

--- src/js/case.js
+++ src/js/case.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+const caseGallery = () => {
   const galleries = document.querySelectorAll('[data-js="gallery-container"]')
 
   galleries.forEach((gallery) => {
@@ -6,9 +6,44 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainImg = gallery.querySelector('[data-js="gallery-main-image"]')
 
     thumbnails.forEach((thumbnail) => {
-      thumbnail.addEventListener('click', function () {
+      thumbnail.addEventListener('click', () => {
         mainImg.src = thumbnail.src
       })
     })
   })
+}
+
+const openCase = () => {
+  const cases = document.querySelectorAll('[data-js="case-container"]')
+
+  cases.forEach((project) => {
+    const caseButton = project.querySelectorAll('[data-js="case-button"]')
+    const text = project.querySelector('.case-line-clamp')
+    const thumbs = project.querySelector(
+      '[data-js="gallery-thumbnail-container"]'
+    )
+
+    caseButton.forEach((button) => {
+      button.addEventListener('click', () => {
+        if (text.classList.contains('case-line-clamp')) {
+          button.innerHTML = `
+            fechar
+            <i class="bi bi-x-lg"></i>
+          `
+        } else {
+          button.innerHTML = `
+            conheça o case
+            <i class="bi bi-arrow-right"></i>
+          `
+        }
+        text.classList.toggle('case-line-clamp')
+        thumbs.classList.toggle('d-none')
+      })
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  caseGallery()
+  openCase()
 })
