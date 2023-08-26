fd517d65b38094c20e09929d3895a507cfef81b2
Marcelo Soares <soares.srp@gmail.com>
2023-08-26 15:04:35 -0300

    feat: clients logo

---
+++ src/js/home.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  slidesPerView: 1,
+  spaceBetween: 72
+})
