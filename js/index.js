aa450a77ab261729883a302db1a225270d6c37b2
Marcelo Soares <soares.srp@gmail.com>
2023-08-25 22:35:26 -0300

    feat: logos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 import '../scss/main.scss'
 
+function addImagesToContainer(imageUrls) {
+  const container = document.querySelector('.contact__clients__logos')
+
+  imageUrls.forEach((url) => {
+    const imgElement = document.createElement('img')
+    imgElement.src = url
+    imgElement.alt = 'logo'
+    container.appendChild(imgElement)
+  })
+}
+
+const imagesArray = new Array(26)
+  .fill(null)
+  .map((_, index) => `./${index + 1}.png`)
+
+addImagesToContainer(imagesArray)
+
 // eslint-disable-next-line no-undef, no-unused-vars
 const swiper = new Swiper('.swiper', {
   navigation: {
